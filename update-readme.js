eb994d57d182728710e6997f24d612c94d271d05
Akhilesh Kr <akacademic05@gmail.com>
2024-01-01 04:14:20 +0530

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
 const fs = require('fs');
+const path = require('path');
 
 axios.get('https://api.quotable.io/random')
   .then(response => {
     const quote = response.data.content;
     const author = response.data.author;
     const date = new Date().toDateString();
-    const existingContent = fs.readFileSync('README.md', 'utf8');
+    const readmePath = path.join(process.env.GITHUB_WORKSPACE, 'README.md');
+    const existingContent = fs.readFileSync(readmePath, 'utf8');
     const newContent = `\n\n## Today's Inspiration (${date})\n\n"${quote}" - ${author}\n`;
-    fs.appendFileSync('README.md', newContent);
+    fs.appendFileSync(readmePath, newContent);
   })
   .catch(error => console.error(error));
